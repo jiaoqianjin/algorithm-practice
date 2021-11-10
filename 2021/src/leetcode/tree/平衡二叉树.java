4fb2c48a57d9cc574d332884a5f1f265810bf4fe
qianjin <1542890862@qq.com>
2021-11-10 14:28:29 +0800

    feat: add balance tree

---
+++ 2021/src/leetcode/tree/平衡二叉树.java
@@ -0,0 +1,27 @@
+package leetcode.tree;
+
+/**
+ * Description：
+ *
+ * @author jiaoqianjin
+ * Date: 2021/11/10 13:52
+ **/
+
+public class 平衡二叉树 {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+
+    }
+}
